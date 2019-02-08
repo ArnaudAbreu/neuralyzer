24b6ddbc3c202f974866bd64200dd4cce8cb2aab
ABREU ARNAUD <aabreu@unistra.fr>
2019-02-08 17:10:02 +0100

    attempt to debug shape in bnn validate

--- neuralyzer/model/bayesian_classifier_model.py
+++ neuralyzer/model/bayesian_classifier_model.py
@@ -107,6 +107,7 @@ class BCLF(Model):
 
         probs = numpy.asarray([self.sess.run([self.labels_distribution.probs], feed_dict=feed_dict) for _ in range(self.post_sampling)])
         mean_probs = numpy.mean(probs, axis=0)
+        print(mean_probs.shape)
         # mean probs is (batch, n_classes)
         logprob = numpy.mean(numpy.log(mean_probs[numpy.arange(mean_probs.shape[0]), y.astype(int)]))
         accuracyval = (numpy.argmax(mean_probs, axis=1) == y.astype(int)).sum()
